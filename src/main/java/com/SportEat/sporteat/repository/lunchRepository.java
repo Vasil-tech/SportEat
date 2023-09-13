df2a046f39ec34b621b307b9a9c7f33049a481ec
Vasil-tech <xamistic02@gmail.com>
2023-09-13 19:58:39 +0300

    Implemented the functionality of adding content to the "Lunch" section

---
+++ src/main/java/com/SportEat/sporteat/repository/lunchRepository.java
@@ -0,0 +1,7 @@
+package com.SportEat.sporteat.repository;
+
+import com.SportEat.sporteat.models.Lunch;
+import org.springframework.data.repository.CrudRepository;
+
+public interface lunchRepository extends CrudRepository<Lunch, Long> {
+}
