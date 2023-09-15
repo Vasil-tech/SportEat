c1dad5cda1c51cc64bb76ee71b5585d7be54e78e
Vasil-tech <xamistic02@gmail.com>
2023-09-15 23:32:43 +0300

    Fixed a bug with adding to the database from the "Breakfast" section

--- src/main/java/com/SportEat/sporteat/models/Breakfast.java
+++ src/main/java/com/SportEat/sporteat/models/Breakfast.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 @Entity
 public class Breakfast {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name, anons, full_text;
 	private int views;
